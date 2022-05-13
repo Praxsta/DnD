8fa128baa2854a392a7f497119e6556705444ad5
Praxsta <praxsta@gmail.com>
2022-05-13 22:27:25 +0100

    Added other stuff into CharacterSheet.py. Added a TODO so someone can work on that.

--- Character/CharacterSheet.py
+++ Character/CharacterSheet.py
@@ -2,6 +2,7 @@ from types import BuiltinMethodType
 from Behaviours import Behaviour
 import random
 
+
 class CharacterProfileSheet:
     def __init__(self, name: str, behaviour: Behaviour = None) -> None:
         """
@@ -14,10 +15,52 @@ class CharacterProfileSheet:
         self.__NAME = name
 
         # If behaviour hasn't been assigned, give it a random one.
-        if behaviour == None:
+        if behaviour is None:
+            # Sorts the Behaviour Enums into a list and picks one at random
             self.__BEHAVIOUR = random.choice(list(Behaviour))
         else:
             self.__BEHAVIOUR = behaviour
+
+        # TODO Create a character generator script where it does Behaviour = random.choice etc rather than putting it
+        #   in here
+
+        # Saving Throws for if you really fuck up.
+        self.saving_throw_strength = 0
+        self.saving_throw_dexterity = 0
+        self.saving_throw_constitution = 0
+        self.saving_throw_intelligence = 0
+        self.saving_throw_wisdom = 0
+        self.saving_throw_charisma = 0
+
+        #######################
+
+        self.__strength = 0
+        self.__dexterity = 0
+        self.__constitution = 0
+        self.__intelligence = 0
+        self.__wisdom = 0
+        self.__charisma = 0
+
+        self.acrobatics = 0
+        self.animal_handling = 0
+        self.arcana = 0
+        self.athletics = 0
+        self.deception = 0
+        self.history = 0
+        self.insight = 0
+        self.intimidation = 0
+        self.investigation = 0
+        self.medicine = 0
+        self.nature = 0
+        self.perception = 0
+        self.performance = 0
+        self.persuasion = 0
+        self.religion = 0
+        self.sleight_of_hand = 0
+        self.stealth = 0
+        self.survival = 0
+
+        self.proficiencies = []
 
     @property
     def behaviour(self):
